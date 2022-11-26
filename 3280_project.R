ceebfd20e9f4d1fc2698733337ab81a164e3b7d2
David Bass <davidbass256@gmail.com>
2022-11-26 01:10:52 -0500

    Add files via upload

--- 3280_project.R
+++ 3280_project.R
@@ -39,45 +39,71 @@ PC_EU <- cbind(PC_EU, pro$Yrot) %>%
          proc_lat="1", proc_long="2") %>%
   mutate(proc_lat = proc_lat + mean(sample_lat),
          proc_long = proc_long + mean(sample_long)) %>%
-  mutate(sample_lat_text_adj = case_when(
+  mutate(sample_long_text_adj = case_when(
+           country == "Germany" ~ -0.5,
+           country == "France" ~ -1.2,
+           country == "Switzerland" ~ 0.2,
+           TRUE ~ 0),
+         sample_lat_text_adj = case_when(
            country == "Spain" ~ -0.5,
            country == "Germany" ~ -0.7,
+           country == "Portugal" ~ 0.8,
+           country == "Italy" ~ -0.8,
+           country == "Austria" ~ -0.8,
+           country == "Netherlands" ~ 0.8,
+           country == "Switzerland" ~ -0.8,
+           country == "France" ~ -0.2,
            TRUE ~ 0),
-         sample_long_text_adj = case_when(
-           country == "Germany" ~ -0.5,
-           TRUE ~ 0))
+         pc_long_text_adj = case_when(
+           country == "Germany" ~ -2.7,
+           country == "Switzerland" ~ -2.7,
+           country == "Austria" ~ 2.2,
+           TRUE ~ 0),
+         pc_lat_text_adj = case_when(
+           country == "Spain" ~ 0.8,
+           country == "Germany" ~ -0.4,
+           country == "Portugal" ~ 0.8,
+           country == "Italy" ~ -0.8,
+           country == "France" ~ -0.8,
+           country == "Austria" ~ -0.6,
+           country == "Netherlands" ~ -0.5,
+           country == "Switzerland" ~ 0.4,
+           TRUE ~ 0),)
 
 map_eu <- map_data("world") %>%
-  mutate(sampled = region %in% 
-           c("Portugal", "Spain", "France", "Italy", "Switzerland", "German", "Austria", "Netherlands"),
-         british_isles = region %in% c("UK", "Ireland", "Algeria", "Tunisia", "Morocco", "Isle of Man"))
+  mutate(hide = region %in% c("UK", "Ireland", "Algeria", "Tunisia", "Morocco", "Isle of Man"))
+
+pc_col <- "#C21010"
+land_col <- "#CFE8A9"
+sea_col <- "#FFFDE3"
 
 # Plot Procrustes analysis results with map in background
 ggplot(PC_EU) + 
   geom_polygon(map_eu,
                mapping=aes(x=long, y=lat, group=group, 
-                           fill=british_isles, color=british_isles)) +
-  scale_fill_manual(values=c("#cccccc", "#acbfe3")) +
-  scale_color_manual(values=c("black", "#acbfe3")) +
-  geom_point(aes(x=sample_long, y=sample_lat), size=3) +
+                           fill=hide, color=hide)) +
+  scale_fill_manual(values=c(land_col, sea_col)) +
+  scale_color_manual(values=c("black", sea_col)) +
+  geom_point(aes(x=sample_long, y=sample_lat), size=4) +
   geom_text(aes(x=sample_long + sample_long_text_adj, 
                 y=sample_lat + sample_lat_text_adj,
                 label=country),
-            size=5) +
-  geom_point(aes(x=proc_long, y=proc_lat), size=3, color="#c40a20") +
-  geom_text(aes(x=proc_long, y=proc_lat, label=country),
-            color="#c40a20", size=5) +
+            size=6, fontface="bold") +
+  geom_point(aes(x=proc_long, y=proc_lat), size=4, color=pc_col) +
+  geom_text(aes(x=proc_long + pc_long_text_adj, 
+                y=proc_lat + pc_lat_text_adj, 
+                label=country),
+            color=pc_col, size=6, fontface="bold") +
   geom_curve(aes(x=sample_long, y=sample_lat,
                  xend=proc_long, yend=proc_lat),
-             arrow=arrow(length=unit(0.1, "cm")),
-             curvature=0.2,
-             size=1) +
-  geom_text(aes(label="Geography of samples", x=-8, y=54), 
-            size=6, hjust=0) +
-  geom_text(aes(label="Geography reconstructed\nfrom genetics", x=-8, y=52.5), 
-            color="#c40a20", size=6, hjust=0) +
+             arrow=arrow(length=unit(0.3, "cm")),
+             curvature=0.2, size=1) +
+  geom_text(aes(label="Geography of samples", x=-9.5, y=54), 
+            size=7, hjust=0) +
+  geom_text(aes(label="Geography reconstructed\nfrom genetics", x=-9.5, y=51.9), 
+            color=pc_col, size=7, hjust=0) +
   theme_classic() +
-  theme(panel.background = element_rect(fill="#acbfe3"),
+  theme(panel.background = element_rect(fill=sea_col),
         legend.position = "none") +
   coord_fixed(xlim=c(-9, 15), ylim=c(36, 54)) + 
   xlab("Longitude / PC 2") +
